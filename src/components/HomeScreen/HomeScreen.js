eb0127fe7c49d29e23b3ccdeb24b4a1a3a8aebd4
Resister-boy <hassanpumped17@gmail.com>
2023-02-15 19:34:10 +0900

    create Row

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
-import styles from './HomeScreen.css';
-import Navigation from "./Navigation/Navigation";
-import Banner from "./Banner/Banner"
+import requests from '../../Request';
+import { Navigation, Banner, Row } from "./index";
 
 const HomeScreen = () => {
   return (
-    <div className={styles.home}>
+    <div className="home">
       <Navigation />
       <Banner />
+      <Row title="Trending Now" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/components/HomeScreen/index.js
@@ -0,0 +1,9 @@
+import Navigation from "./Navigation/Navigation";
+import Banner from "./Banner/Banner";
+import Row from "./Row/Row";
+
+export {
+  Navigation,
+  Banner,
+  Row
+}
